a6536a5db61f1b80f60f1deaefa9653c01010d46
sagarulli <sagarulli5@gmail.com>
2024-08-23 20:16:53 +0530

    Second

---
+++ Variable.java
@@ -0,0 +1,14 @@
+class Variable {
+	public static void main(String[] args) {
+		System.out.println("Mobile_cost is "+15000+" Mobile model_name is Note 11"+" Mobile_colour is Matte black");
+		System.out.println("Course_name is Computer Science"+" Unvir_name is AIT"+" Exam_cost"+1680);
+		System.out.println("Car_cost "+1500000+" Car model_name is Volkswagen"+" Car type is Petrol");
+		System.out.println("Laptop_name is Elitebook "+"Laptop cost is "+20000+" Laptop_brand is HP");
+		System.out.println("School_name is Gurudeva "+" School grade is "+'A'+" School strength is "+1000);
+		System.out.println("Home_name is Shri "+"Home_cost is "+1000000+" Home_colour is Skyblue");
+		System.out.println("TV_cost is "+20000+" TV_brand is Samsung"+" TV type is LED");
+		System.out.println("emp_id is "+1+" emp_sal is "+40000+" emp_grade is "+'B');
+		System.out.println("Bike_cost is "+80000+" Bike_brand is Honda"+" Bike_colour is Black&White");
+		System.out.println("Cloth_colour is black "+" cloth_cost is "+1000+" cloth_size is S");
+	}
+}
