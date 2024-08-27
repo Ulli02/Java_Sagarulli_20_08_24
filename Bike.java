da92792c8a6b5da1bf1024166706442bb26568c6
sagarulli <sagarulli5@gmail.com>
2024-08-27 20:36:15 +0530

    variable

---
+++ Bike.java
@@ -0,0 +1,8 @@
+class Bike {
+	public static void main(String[] args) {
+		long B_cost = 80000;
+		String B_brand = "Honda";
+	 	String B_color = "Black & White";
+		System.out.println("Bike Cost: "+B_cost+" Bike Brand: "+B_brand+" Bike Color: "+B_color);
+	}
+}
