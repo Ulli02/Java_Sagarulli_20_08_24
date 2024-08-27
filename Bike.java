f4b220d845d22bb3cb7ec555355f774e3f8d5c89
sagarulli <sagarulli5@gmail.com>
2024-08-27 20:43:03 +0530

    global variable

--- Bike.java
+++ Bike.java
@@ -1,8 +1,15 @@
 class Bike {
+<<<<<<< HEAD
 	public static void main(String[] args) {
 		long B_cost = 80000;
 		String B_brand = "Honda";
 	 	String B_color = "Black & White";
+=======
+	static long B_cost = 80000;
+	static String B_brand = "Honda";
+	static String B_color = "Black & White";
+	public static void main(String[] args) {
+>>>>>>> f7af90c (global variable)
 		System.out.println("Bike Cost: "+B_cost+" Bike Brand: "+B_brand+" Bike Color: "+B_color);
 	}
 }
